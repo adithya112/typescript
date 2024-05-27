0ffc795d9abc65cb80dff5f44371aad6dbdcf629
adithya <adithyag.s@incture.com>
2024-05-27 11:36:03 +0530

    second commit

---
+++ src/tutorial.ts
@@ -0,0 +1,25 @@
+let awesome: string = "shakeAndbake";
+awesome = "something";
+awesome = awesome.toUpperCase();
+console.log(awesome);
+
+// awesome = 20;
+
+let amount: number = 10;
+// amount = "pants";
+
+let isAwesome: boolean = true;
+isAwesome = false;
+// isAwesome = "shakeAndBake";
+
+//challenges
+let str: string = "adithya";
+str = str.toUpperCase();
+console.log(str);
+
+let num: number = 29;
+num = num + 10;
+console.log(num);
+
+let bool = true; //type inference
+console.log(bool === true);
